95d5e249703c5ada591f4eeb4c207add44f186ec
mrjohnny1215 <mrjohnny1215@users.noreply.github.com>
2026-09-12 16:10:32 +0700

    fix: use transparent logo asset

--- src/components/AllRentalLogo.jsx
+++ src/components/AllRentalLogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function AllRentalLogo({ src = '/images/allrental-logo.gif', alt = 'AllRental' }) {
+export default function AllRentalLogo({ src = '/images/allrental-logo-transparent.gif', alt = 'AllRental' }) {
   return (
     <div className="allRentalBrand" aria-label="AllRental">
       <img className="allRentalMark" src={src} alt={alt} draggable={false} />
